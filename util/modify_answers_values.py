289630d1e61c2fba2ca0ac9a1b84291346b3df34
michimichiamo <michele.iannello96@gmail.com>
2022-01-11 18:02:57 +0100

    fixed answers, added Dataset

--- util/modify_answers_values.py
+++ util/modify_answers_values.py
@@ -1,4 +1,4 @@
-def move_answers_position(offsets, answer):
+def move_answers_position(input_ids, offsets, answer):
   new_answers = []
   all_inclusive = 0
   not_all_inclusive = 0
@@ -12,9 +12,10 @@ def move_answers_position(offsets, answer):
     ans_start = answer[0][index]
     ans_end = answer[1][index]
     offset = offsets[index]
+    input_id = input_ids[index]
 
     # the search starts after the question, so after the first 102 tag
-    for i in range(offset.index(102), len(offset)):
+    for i in range(input_id.index(102), len(offset)):
       of = offset[i]
       range_offset = list(range(of[0], of[1]+1))
 
