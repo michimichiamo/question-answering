459fc05ed8d1efac3a890a706ffab90ee99566fc
Alessandro Pavesi <pavesialessandro@live.it>
2022-01-11 16:57:24 +0100

    Fixed answer

--- modify_answers_values.py
+++ modify_answers_values.py
@@ -19,12 +19,11 @@ def move_answers_position(offsets, answer):
       range_offset = list(range(of[0], of[1]+1))
 
       if ans_start in range_offset:
-
         # save the word index i as the answer start
         start_founded = True
         start_index_word = i
-      if ans_end in range_offset:
 
+      if ans_end in range_offset:
         # save the word index i as the answer end
         end_founded = True
         end_index_word = i
@@ -39,32 +38,9 @@ def move_answers_position(offsets, answer):
       continue
     else:
       # here's the problems
+      not_all_inclusive += 1
+      new_answers.append((0, 0))
+      continue
 
-      # if the answer isn't included at all -> put (0,0) as answer index
-      if not start_founded and not end_founded:
-        not_all_inclusive += 1
-        new_answers.append((0, 0))
-        continue
-
-      # if the start answer is included and not the end -> answer_start remains, answer end become the end
-      if start_founded and not end_founded:
-        print("Start inclusive")
-        start_inclusive += 1
-        print((start_index_word, end_index_word))
-        new_answers.append((start_index_word, len(offset)))
-        continue
-
-      # if the start answer isn't included and the end is -> answer_start 0, answer end takes word index
-      if not start_founded and end_founded:
-        print("End inclusive")
-        print(index)
-        print(offset)
-        print(ans_start)
-        print(ans_end)
-        end_inclusive += 1
-        print((start_index_word, end_index_word))
-        new_answers.append((0, end_index_word))
-        continue
-
-  print("stats: (all, noth, start, end): {}".format((all_inclusive, not_all_inclusive, start_inclusive, end_inclusive)))
+  print("stats: (all, noth): {}".format((all_inclusive, not_all_inclusive)))
   return new_answers
